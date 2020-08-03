0a02dc421fd00c6110b4390f597df506582bf1de
Christian Berg <cberg@stud.hs-heilbronn.de>
2020-08-03 10:20:30 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Add tv streams to a list and select them via a menu dialog.
+Fork this repo to add your own list.
+Install github cli to create issues in case one url is e.g. outdated.
---
+++ dialogtv.sh
@@ -0,0 +1,20 @@
+#
+
+function report_github(){
+    dialog --yesno "Can't start stream:\n"'"'"$(echo "$STREAM" | cut -d " " -f2-)"'"'".\nLogin to & create issue on GitHub?" 0 0
+    [ $? -eq 0 ] && { 
+        gh issue create --title "[Created by script] Can't start $(echo "$STREAM" | cut -d " " -f2-) stream" --body "$(echo "$STREAM" | cut -d " " -f1) may not exist anymore!" || echo "Install github-cli!"
+    } && exit
+}
+
+tvlist=$(while read -r url name; do echo "$url $name"°"$name"°"$url"°; done < tvlist; unset IFS)
+
+while true
+do
+    IFS="°"
+    STREAM=$(dialog --backtitle "dialogtv.sh" --title "Stream list" --stdout --no-ok --no-cancel --item-help --default-item "$STREAM" --no-tags --menu "Select which stream you want to watch.\nIt might take a while to start." 0 0 0 \
+    $tvlist)
+    unset IFS
+    mpv $(echo "$STREAM" | cut -d " " -f1)
+    [ $? -eq 2 ] && report_github "$STREAM"
+done
